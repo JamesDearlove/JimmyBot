59e79a65fd4adb062b2ba6e0ebfe34b393846b02
Kenton Lam <kenton_lam@outlook.com>
2019-03-01 10:39:20 +1000

    Handle error messages for !mcstatus.

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ import feedparser
 import requests
 import bom
 
-from mcstatus import MinecraftServer
 from datetime import datetime, time
 from random import choice
 
@@ -179,33 +178,11 @@ async def commit(ctx):
 @bot.command()
 async def mcstatus(ctx, *, inputArg = 'tms.jamesdearlove.com'):
     """Gets information about a Minecraft server."""
-    server = MinecraftServer.lookup(inputArg)
 
-    status = server.status() 
-
-    address = f'{server.host}:{server.port}'
-    # TODO: Regex away MC formatting codes
-    motd = status.description['text'] if type(status.description) is dict else status.description.strip()
-
-    version = status.version.name
-    software = version
-
-    ping = f'{status.latency} ms'
-
-    players = f'{status.players.online}/{status.players.max}'
-
-    if status.players.sample:
-        players = f'({players}) ' + ', '.join([x.name for x in status.players.sample])
-
-    full_status = [
-        '**Server:** ' + address,
-        '**Message:** ' + motd,
-        '**Version:** ' + software,
-        '**Ping:** ' + ping,
-        '**Players:** ' + players
-    ]
-
-    message = '\n'.join(full_status)
+    try:
+        message = utils.get_mcstatus_text(inputArg)
+    except Exception as e:
+        message = f'**Error:** {e}'
 
     await ctx.send(message)
 
